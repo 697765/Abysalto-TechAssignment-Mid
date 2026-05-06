89224a07edb537c462de7542727f0af41552b53b
emavr <emavrdoljak02@gmail.com>
2026-05-07 01:58:47 +0200

    add CartRepository implementation

---
+++ AbySalto.Mid.Infrastructure/Repository/CartRepository.cs
@@ -0,0 +1,25 @@
+﻿using AbySalto.Mid.Application.Cart;
+using AbySalto.Mid.Domain.Entities;
+using AbySalto.Mid.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AbySalto.Mid.Infrastructure.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CartRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Cart?> GetByUserIdAsync(string userId)
+        {
+            return await _context.Carts
+                .Include(x => x.Items)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+        }
+    }
+}
